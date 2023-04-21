1a59deadb228749000860e7cf654189b513ad64e
scheronlini <scheronlini@gmail.com>
2023-04-21 18:25:12 -0300

    first commit

---
+++ greeting/src/main/java/com/scheronlini/greeting/Startup.java
@@ -0,0 +1,13 @@
+package com.scheronlini.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
